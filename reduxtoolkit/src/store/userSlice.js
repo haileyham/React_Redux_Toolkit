3b08a02999ba7dbf0d6238a6951c20e5991e1dd5
hailey ham <roserealslow@gmail.com>
2023-06-06 16:31:35 +0900

    주석추가

--- reduxtoolkit/src/store/userSlice.js
+++ reduxtoolkit/src/store/userSlice.js
@@ -8,11 +8,11 @@ let user = createSlice({
       state.name = "ham" // name의 hailey를 ham으로 변경 
     },
     changeAge(state, actions) {
-      state.age += actions.payload
+      state.age += actions.payload //입력받은 값으로 버튼 누를때마다 age 증가
     }
   }
 })
 
 export let { changeName, changeAge } = user.actions //위에서 만든 변경함수 export
-export { user }
+export { user } //store.js에서 import하기 위해서
 

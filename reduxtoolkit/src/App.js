5737e645ff88cb3351e6ef24a83c603b737b58ff
hailey ham <roserealslow@gmail.com>
2023-06-06 15:32:37 +0900

    [store.js파일]createSlice,이름변경함수 생성,export / [App.js파일]등록된 state import 및 변경함수 실행 / 주석간단설명

--- reduxtoolkit/src/App.js
+++ reduxtoolkit/src/App.js
@@ -1,11 +1,20 @@
+import { useSelector, useDispatch } from "react-redux";
+import { changeName } from "./store";
+
 function App() {
+
+  //등록된 state 다 가져옴
+  // const state = useSelector((state) => { return state });
+
+  // 등록된 state 중에서 user 가져옴
+  const state = useSelector((state) => { return state.user });
+  const dispatch = useDispatch();
 
   return (
     <div>
-      안녕월드얌~
-      Hello World
-      Redux_Toolkit ~
-      Let's go!
+      {/* 버튼을 누르면 hailey에서 ham으로 변경되는 것을 확인할 수 있음 */}
+      <h1>{state.name}</h1>
+      <button onClick={() => { dispatch(changeName()) }}>Name Change</button>
     </div>
   );
 }
--- reduxtoolkit/src/store.js
+++ reduxtoolkit/src/store.js
@@ -1,6 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+
+let user = createSlice({
+  name: 'user',
+  initialState: { name: "hailey", age: 1 },
+  reducers: {
+    changeName(state) {
+      state.name = "ham" // name의 hailey를 ham으로 변경 
+    }
+  }
+})
+
+export let { changeName } = user.actions //위에서 만든 변경함수 export
 
 export default configureStore({
   reducer: {
+    user: user.reducer //user createSlice state를 등록(useSelector에서 이곳에 등록되어 있는 state들을 받아옴)
   }
 })
